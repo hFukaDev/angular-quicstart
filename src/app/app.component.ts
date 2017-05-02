ab5860099970d9464713405209df59ca5596d827
unknown <non@mail.com>
2017-05-02 22:32:03 +0900

    コンポーネントクラス
    コンポーネントはHTMLページ上の部品を表す。
    angular2アプリケーションでは、最低でも一つのコンポーネントクラスが必要。
    コンポーネントクラスには@Componentで示されるデコレーターが付加されている。
    このデコレータは以下の引数を受け取る。
     selector -> HTML上でコンポーネントを展開するためのタグを指定。
     template -> 展開されたHTMLの内容を示すテンプレート
    つまりComponentデコレータはHTMLとクラスのバインド方法そ指定している。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
+/*
+ * angularアプリケーションはコンポーネントで構成されています。
+ * コンポーネントは、HTMLテンプレートとそれにバインドされるクラスの組み合わせ。
+ */
+
+// Componentでコレータを使うのでインポートする。
 import { Component } from '@angular/core';
 
+/*
+ * Componentデコレータはコンポーネントクラスに対して必ずつける必要がある。
+  */
 @Component({
+  // selectorプロパティはangularに対して、コンポーネントを表すタグの名前を示す。
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // templateはselectorで示されたタグが、DOM上でどのように展開されるかを表す。
+  // {{ }}で囲まれた名前はコンポーネントクラスのプロパティと対応する。
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title='Tour of Heroes';
+  hero='Windstorm';
+}
