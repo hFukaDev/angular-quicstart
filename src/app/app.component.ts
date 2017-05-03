edd108f47ffefc0f63e2b24810d57148c5736056
unknown <non@mail.com>
2017-05-03 16:19:59 +0900

    リストデータを列挙する
    *ngForディレクティブを使えば、リスト形式のデータを列挙することができる。
    今回はヒーローオブジェクトの配列をliタグを使って一覧表示している。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,19 @@ export class Hero {
   id: number;
   name: string;
 }
+// ヒーロー一覧に表示するデータ
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 
 
 /*
@@ -28,6 +41,12 @@ export class Hero {
   // input/output両方に対応しているのでtwo way bindingと言われる。
   // ngModuleを使うにはFormsModuleを使う必要がある。
   // そのためapp.module.tsで@angular/formsからインポートする必要がある。
+  //
+  // *ngForディレクティブはリストデータを列挙する。
+  // <tag *ngFor="let 変数名 of リストデータ">
+  //  :
+  // </tag>
+  // tagの中が繰り返されるのではなくtagとその中身が繰り返される。
   template: `
     <h1>{{title}}</h1>
     <div>
@@ -37,14 +56,22 @@ export class Hero {
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div><label>name: </label>{{hero.name}}</div>
-
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     `,
+
 })
 export class AppComponent  {
   title='Tour of Heroes';
   // ヒーローを文字列のデータから、追加したHeroオブジェクトに変更
   hero: Hero = {
-  id: 1,
-  name: 'Windstorm'
+    id: 1,
+    name: 'Windstorm'
   };
+  // ヒーロー一覧に表示するデータ
+  heroes = HEROES;
 }
